0a0a4f7f58aa427b20a5f40187f23c9fe43e95ec
Reallife101 <ryan.ngoon@gmail.com>
2021-06-02 20:38:14 -0700

    Add basic typing and unit testing

--- assignment5.jl
+++ assignment5.jl
@@ -1,24 +1,59 @@
+#Import unit tests
+using Test
+
 #----------------Data Definitions--------------
+#;ExprC: Function definitions, application of functions, ifs, real, str and sym
+struct LamC{T} 
+	args::Vector{String}
+	body::T
+end
 
+struct AppC{T} 
+	fun::T
+	arg::Vector{T}
+end
+
+struct IfC{T}
+	a::T
+	b::T
+	c::T
+end
+
+ExprC = Union{Number, Bool, String, LamC, AppC, IfC}
+
+#Values
+Value = Union{Number, String, Bool}
+
+#Environment: List of Binding
 struct Binding
-				name
-				value
-			end
+	name::String
+	value:: Value
+end
+Environment = Vector{Binding}
 
-struct IfC
-           a
-           b
-           c
-       end
 #-------------- Base Environment --------------
-base-env = [Binding("true", true),
-			Binding("false", false),
-			Binding("+", +),
-			Binding("-", -),
-			Binding("*", *),
-			Binding("/", /)]
+baseenv = [Binding("true", true),
+			Binding("false", false)]
+			# Binding("+", +),
+			# Binding("-", -),
+			# Binding("*", *),
+			# Binding("/", /)]
+
+#---------- Interp ---------
+function interp(e::ExprC, env::Environment)
+	if e isa String || e isa Number 
+		return e
+	else e isa IfC
+		return if e.a e.b else e.c end
+	end
+end
 
 #-------------- Testing ----------------
+@test typeof(Binding("true", true)) == Binding
+@test 3.121321 isa ExprC
 
-binding1 =  Binding("true", true)
-print(typeof(binding1))
+#Test interp 
+@test interp(1, baseenv) == 1 
+@test interp("hi", baseenv) == "hi" 
+@test interp(IfC{ExprC}(true, 1, 2), baseenv) == 1 
+
